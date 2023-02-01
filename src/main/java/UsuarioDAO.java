f5b2902462d2f5b995edfa1269035126cc80594f
liasantos <liasilva77@acad.charqueadas.ifsul.edu.br>
2023-02-01 20:01:12 -0300

    .

--- src/main/java/UsuarioDAO.java
+++ src/main/java/UsuarioDAO.java
@@ -1,6 +1,9 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UsuarioDAO {
     private Connection conexao;
@@ -22,6 +25,67 @@ public class UsuarioDAO {
         } catch (SQLException sqle) {
             System.out.println("Não foi possível inserir");
         }catch (Exception e) {
+            System.out.println("Erro desconhecido");
+        }
+    }
+
+    public List<Usuario> listar() {
+        List<Usuario> lista = new ArrayList<>();
+        try {
+            ResultSet rs = conexao.createStatement().executeQuery("select * from usuarios");
+            Usuario u;
+            while (rs.next()) {
+                u = new Usuario();
+                u.setId(u.getLong("id"));
+                u.setNome(u.getString("nome"));
+                u.setCpf(u.getString("cpf"));
+                u.setTelefone(u.getString("telefone"));
+                u.setEmail(u.getString("email"));
+                u.setSenha(u.getString("senha"));
+                lista.add(u);
+            } return lista;
+        } catch(SQLException sqle) {
+            System.out.println("Não foi possível processar a lista");
+            return null;
+        } catch(Exception e) {
+            System.out.println("Erro desconhecido");
+            return null;
+        }
+
+    }
+
+    public void alterar(Usuario usuario) {
+        try {
+            String sql = "update usuarios set nome = ?, cpf = ?, telefone = ?, email = ?, email = ?, senha = ? where id = ?";
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+            stmt.setString(1, usuario.getNome());
+            stmt.setString(2, usuario.getCpf());
+            stmt.setString(3, usuario.getTelefone());
+            stmt.setString(4, usuario.getEmail());
+            stmt.setString(5, usuario.getSenha());
+            stmt.setLong(6, usuario.getId());
+            System.out.println(stmt.toString());
+            stmt.execute();
+            stmt.close();
+
+        } catch(SQLException sqle){
+            System.out.println("Não foi possível alterar");
+            sqle.printStackTrace();
+        } catch(Exception e){
+            System.out.println("Erro desconhecido");
+        }
+    }
+
+    public void remover(Usuario usuario) {
+        try {
+            String sql = "delete from usuarios where id = ?";
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+            stmt.setLong(1, usuario.getId());
+            stmt.execute();
+            stmt.close();
+        } catch(SQLException sqle) {
+            System.out.println("Não foi possível remover");
+        } catch(Exception e) {
             System.out.println("Erro desconhecido");
         }
     }
